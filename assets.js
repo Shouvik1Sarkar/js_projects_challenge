7fed3ccb33f9f579a867b3268149c3e5faf0bc50
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-11-12 01:35:00 +0530

    Project 7, currency converter, set up, html css. start js

---
+++ assets.js
@@ -0,0 +1,1 @@
+export const api_key = ec6e61299c2112780bd0b60c;
---
+++ project_06_currency_converter/script.js
@@ -0,0 +1,2 @@
+import { api_key } from "../assets.js";
+const api_url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
